28425883f006055fdd3ac13d9789bd51ee286954
daniel <presidente152016@outlook.com>
2021-11-01 23:06:51 -0500

    Agregando el controller, servicio, resources, mapping, repositorio de Advertisements.

---
+++ src/main/java/com/AdoptMeYa/Back/adoptme/domain/model/entity/Advertisement.java
@@ -0,0 +1,56 @@
+package com.AdoptMeYa.Back.adoptme.domain.model.entity;
+
+import com.AdoptMeYa.Back.shared.domain.model.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Table(name="advertisements")
+public class Advertisement extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private Long userId;
+
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    @Column(unique = false)
+    private String title;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    @Column(unique = false)
+    private String description;
+
+    @NotNull
+    @NotBlank
+    @Column(unique = false)
+    private Float discount;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 300)
+    @Column(unique = false)
+    private String urlToImage;
+
+    @NotNull
+    @NotBlank
+    @Column(unique = false)
+    private Boolean promoted;
+
+}
---
+++ src/main/java/com/AdoptMeYa/Back/adoptme/domain/service/AdvertisementService.java
@@ -0,0 +1,18 @@
+package com.AdoptMeYa.Back.adoptme.domain.service;
+
+import com.AdoptMeYa.Back.adoptme.domain.model.entity.Advertisement;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface AdvertisementService {
+    Advertisement listAdvertisementsByUserId(Long userId);
+    Page<Advertisement> listAdvertisements(Pageable pageable);
+    ResponseEntity<?> delete(Long advertisementId);
+    Advertisement create(Advertisement advertisement);
+    Advertisement update(Long advertisementId, Advertisement request);
+    List<Advertisement>ListAllAdvertisements();
+}
