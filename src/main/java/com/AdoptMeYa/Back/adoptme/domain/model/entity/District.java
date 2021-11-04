dcb889faa08a49e5a0fb30653106b222edd2892c
dylandSteven <stevendylandsaldana@gmail.com>
2021-11-04 01:28:46 -0500

    Adding , service about districts , also mapping , domain, etc

---
+++ src/main/java/com/AdoptMeYa/Back/adoptme/domain/model/entity/District.java
@@ -0,0 +1,31 @@
+package com.AdoptMeYa.Back.adoptme.domain.model.entity;
+import com.AdoptMeYa.Back.shared.domain.model.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import javax.xml.transform.Source;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Table(name ="districts")
+
+public class District extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String district;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+
+}
